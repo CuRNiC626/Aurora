12dc99585f9b56ed80746313fc1b90a3dd9a3887
Потапов Дмитрий <dpotapow1234@gmail.com>
2026-03-18 21:08:37 +0300

    Vector3Int Update

--- Math/Core/Vector3Int.cs
+++ Math/Core/Vector3Int.cs
@@ -6,10 +6,92 @@ using System.Threading.Tasks;
 
 namespace Aurora.Math.Core
 {
-    public struct Vector3Int
+    using System;
+
+    public struct Vector3Int : IEquatable<Vector3Int>
     {
         public int X;
         public int Y;
         public int Z;
+
+        #region Конструкторы
+
+        public Vector3Int(int x, int y, int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        #endregion
+
+        #region Константы
+
+        public static readonly Vector3Int Zero = new Vector3Int(0, 0, 0);
+        public static readonly Vector3Int One = new Vector3Int(1, 1, 1);
+
+        public static readonly Vector3Int UnitX = new Vector3Int(1, 0, 0);
+        public static readonly Vector3Int UnitY = new Vector3Int(0, 1, 0);
+        public static readonly Vector3Int UnitZ = new Vector3Int(0, 0, 1);
+
+        #endregion
+
+        #region Операторы
+
+        public static Vector3Int operator +(Vector3Int a, Vector3Int b)
+            => new Vector3Int(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+
+        public static Vector3Int operator -(Vector3Int a, Vector3Int b)
+            => new Vector3Int(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+
+        public static Vector3Int operator *(Vector3Int a, int scalar)
+            => new Vector3Int(a.X * scalar, a.Y * scalar, a.Z * scalar);
+
+        public static Vector3Int operator *(int scalar, Vector3Int a)
+            => a * scalar;
+
+        public static Vector3Int operator /(Vector3Int a, int scalar)
+            => new Vector3Int(a.X / scalar, a.Y / scalar, a.Z / scalar);
+
+        public static bool operator ==(Vector3Int a, Vector3Int b)
+            => a.Equals(b);
+
+        public static bool operator !=(Vector3Int a, Vector3Int b)
+            => !a.Equals(b);
+
+        #endregion
+
+        #region Методы
+
+        public int LengthSquared()
+            => X * X + Y * Y + Z * Z;
+
+        public double Length()
+            => Math.Sqrt(LengthSquared());
+
+        public static int Dot(Vector3Int a, Vector3Int b)
+            => a.X * b.X + a.Y * b.Y + a.Z * b.Z;
+
+        #endregion
+
+        #region Equality
+
+        public bool Equals(Vector3Int other)
+            => X == other.X && Y == other.Y && Z == other.Z;
+
+        public override bool Equals(object obj)
+            => obj is Vector3Int other && Equals(other);
+
+        public override int GetHashCode()
+            => X ^ (Y << 2) ^ (Z >> 2);
+
+        #endregion
+
+        #region ToString
+
+        public override string ToString()
+            => $"({X}, {Y}, {Z})";
+
+        #endregion
     }
 }
